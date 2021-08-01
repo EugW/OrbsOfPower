eb5501f8ed9ee73ffd08cb4ccc3d1bd6cc45f5df
EugW <wug.bond@gmail.com>
2021-08-01 16:56:11 +0500

    readme.md & threads

--- OrbsOfPower/OrbsOfPower.cpp
+++ OrbsOfPower/OrbsOfPower.cpp
@@ -26,19 +26,23 @@ void flt() {
 }
 
 int main(int argc, char** argv) {
+    int threads = 0;
     int mode = 0;
     int delay = 0;
     int key = 0;
     std::string filter;
-    if (argc > 4) {
-        mode = atoi(argv[1]);
-        delay = atoi(argv[2]);
-        key = atoi(argv[3]);
-        filter = std::string(argv[4]);
+    if (argc > 5) {
+        threads = atoi(argv[1]);
+        mode = atoi(argv[2]);
+        delay = atoi(argv[3]);
+        key = atoi(argv[4]);
+        filter = std::string(argv[5]);
     }
     else {
-        std::cout << "USE: OrbsOfPower.exe [mode 0|1] [delay int] [key vk_num] [filter \"string\"]" << std::endl;
+        std::cout << "USE: OrbsOfPower.exe [threads int] [mode 0|1] [delay int] [key vk_num] [filter \"string\"]" << std::endl;
         std::cout << "### MANUAL SETUP ###" << std::endl;
+        std::cout << "### ENTER THREADS ###" << std::endl;
+        std::cin >> threads;
         std::cout << "### ENTER MODE ###" << std::endl;
         std::cout << "### 0 - AUTO (AUTO DELAY AFTER BUTTON PRESS) ###" << std::endl;
         std::cout << "### 1 - MANUAL (HOLD DOWN=LAG) ###" << std::endl;
@@ -55,18 +59,23 @@ int main(int argc, char** argv) {
             key = VkKeyScanExA(t, LoadKeyboardLayoutA("00000409", 0x00000001));
         }
         std::cout << "### ENTER FILTER ###" << std::endl;
+        std::cout << "udp.SrcPort == 3074" << std::endl;
+        std::cout << "udp.SrcPort >= 27000 and udp.SrcPort <= 27100" << std::endl;
         std::cin >> filter;
     };
-    std::cout << "FILTER SET " << filter << std::endl;
+    std::cout << "THREADS SET " << threads << std::endl;
     std::cout << "MODE SET " << mode << std::endl;
     std::cout << "DELAY SET " << delay << std::endl;
     std::cout << "KEYBIND SET(INT,HEX) " << key << ",0x" << std::hex << key << std::endl;
+    std::cout << "FILTER SET " << filter << std::endl;
     handle = WinDivertOpen(filter.c_str(), WINDIVERT_LAYER_NETWORK, 0, 0);
     if (handle == INVALID_HANDLE_VALUE) {
         std::cout << "Couldn't open handle" << std::endl;
         exit(0);
     }
-    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)flt, NULL, 0, NULL);
+    for (int i = 0; i < threads; i++) {
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)flt, NULL, 0, NULL);
+    }
     if (mode == 0) {
         while (true) {
             if (GetAsyncKeyState(key)) {
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Setup #
+    %PathToExe% [number of threads >0] [mode 1 or 0] [key number(virtual key code)] [filter (windivert filter syntax)]
+# Useful examples #
+Mobs will not see you and mechanics won't work on ya (3 minute limit, kick after)
+
+    %PathToExe% 12 1 0 192 "udp.SrcPort == 3074"
+Avtivate right before Orbs spawn and they will be duplicated, activate in time with shot in pvp and you will file multiple shots
+
+    %PathToExe% 12 1 0 192 "udp.SrcPort >= 27000 and udp.SrcPort <= 27100"
+Automatic Orbs dup for Warlock Well Of Radiance (just press F)
+
+    %PathToExe% 12 0 900 70 "udp.SrcPort >= 27000 and udp.SrcPort <= 27100"
+Automatic Orbs dup for Titan Bubble (just press F)
+
+    %PathToExe% 12 0 500 70 "udp.SrcPort >= 27000 and udp.SrcPort <= 27100"
